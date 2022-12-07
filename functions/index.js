cb52610cc090aa11626368035fe55a22a4804ad5
getserved <getserved06@gmail.com>
2022-12-07 11:42:21 +1100

    Remove unused function
    Remove unused function

--- functions/index.js
+++ functions/index.js
@@ -5,23 +5,11 @@ const admin = require("firebase-admin");
 const cors = require("cors")({origin: true});
 admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-exports.helloWorld = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    res.send("Hello from Firebase!");
-  });
-});
-
 exports.addDoc = functions.https.onRequest((req, res) => {
   cors(req, res, async () => {
     const payload = req.body;
 
     // eslint-disable-next-line max-len
     const writeResult = await admin.firestore().collection("mamamia").add(payload);
-
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
   });
 });
